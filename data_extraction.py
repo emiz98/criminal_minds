e438ad656cef9d32a091b1bd076f41de61aea28d
Jayzeen <janithjayasinghe.nipuna@gmail.com>
2022-06-19 21:18:48 +0530

    call type one hot encoded

--- data_extraction.py
+++ data_extraction.py
@@ -1,10 +1,10 @@
 import tabula as tab
 import pandas as pd
 
-# pdf = tab.read_pdf("./docs/1655367558381.pdf", pages='all')
-# pdf2 = tab.read_pdf("./docs/1655367558398.pdf", pages='all')
-# pdf3 = tab.read_pdf("./docs/1655367558416.pdf", pages='all')
-# pdf4 = tab.read_pdf("./docs/1655367558435.pdf", pages='all')
+pdf = tab.read_pdf("../docs/1655367558381.pdf", pages='all')
+pdf2 = tab.read_pdf("../docs/1655367558398.pdf", pages='all')
+pdf3 = tab.read_pdf("../docs/1655367558416.pdf", pages='all')
+pdf4 = tab.read_pdf("../docs/1655367558435.pdf", pages='all')
 
 
 # For 381 pdf
@@ -14,28 +14,46 @@ def data_pipeline(pdf):
     dropped = name.drop(columns = ['SEQ','Cell Id','Lac','Cell Name'])
     dropped['imsi'] = 0
     dropped.columns = ['caller','receiver', 'call_type','date_time','duration','imei','imsi']
-    print(dropped)
+    
+    # one hot encoding call_type
+    converted = pd.get_dummies(dropped['call_type'])
+    combined = pd.concat((converted,dropped), axis=1)
+    combined = combined.drop(["call_type"] ,axis=1)
+    result = combined.rename(columns={"SMS-Incoming": "call_type"})
+    print(result)
     
 
 # For 398 pdf
 def data_pipeline2(pdf):
     df = pd.concat(pdf)
-    name = df.head(5)
-    dropped = name.drop(columns = ['No','Cell ID','Base station'])
+    dropped = df.drop(columns = ['No','Cell ID','Base station'])
     dropped['imsi'] = 0
     dropped.columns = ['imei','caller','receiver','date_time','call_type','duration','imsi']
-    print(dropped)
+    
+    # one hot encoding call_type
+    converted = pd.get_dummies(dropped['call_type'])
+    combined = pd.concat((converted,dropped), axis=1)
+    combined = combined.drop(["call_type","ORIGIN"] ,axis=1)
+    result = combined.rename(columns={"TERMI": "call_type"})
+    print(result)
     
 
 # For 416 pdf
 def data_pipeline3(pdf):
     df = pd.concat(pdf)
-    name = df.head(5)
+    name = df.head(30)
     dropped = name.drop(columns = ['Cell1','Cell2','Type SMSC'])
     dropped["date_time"] = dropped[["Date", "Time"]].apply(" ".join, axis=1)
     merged = dropped.drop(columns = ['Date', 'Time'])
     merged.columns = ['caller','receiver','duration','call_type','imei','imsi','date_time']
-    print(merged)
+    merged = merged[merged["call_type"].str.contains("Incoming SMS-SMT|Outgoing SMS-SMS") == False]
+    
+    # one hot encoding call_type
+    converted = pd.get_dummies(merged['call_type'])
+    combined = pd.concat((converted,merged), axis=1)
+    combined = combined.drop(["call_type"] ,axis=1)
+    result = combined.rename(columns={"Incoming Call-MTC": "call_type"})
+    print(result)
 
 
 # For 435 pdf
@@ -46,14 +64,18 @@ def data_pipeline4(pdf):
     dropped["date_time"] = dropped[["Date", "Time"]].apply(" ".join, axis=1)
     merged = dropped.drop(columns = ['Date', 'Time'])
     merged.columns = ['caller','receiver', 'call_type','duration','imei','imsi','date_time']
-    print(merged)
+    converted = pd.get_dummies(merged['call_type'])
+    combined = pd.concat((converted,merged), axis=1)
+    combined = combined.drop(["call_type","Incoming"] ,axis=1)
+    result = combined.rename(columns={"Outgoing": "call_type"})
+    print(result)
     
     
 # data_pipeline(pdf)
 # print('-----------------------------------------')
 # data_pipeline2(pdf2)
 # print('-----------------------------------------')
-# data_pipeline3(pdf3)
+data_pipeline3(pdf3)
 # print('-----------------------------------------')
 # data_pipeline4(pdf4)
 
