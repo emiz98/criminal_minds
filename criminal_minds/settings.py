db1fcd58ca135440d0b5cfeccd0509ae3a733a4a
emiz98 <64089619+emiz98@users.noreply.github.com>
2022-06-20 13:28:31 +0530

    Heroku Deployment #5

--- criminal_minds/settings.py
+++ criminal_minds/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import sys
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,3 +157,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
